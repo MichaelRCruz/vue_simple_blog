c35eaba002a9b664590d0bac1a3ba85b559917ad
Michael Cruz <michael.cruz@uwalumni.com>
2017-01-31 02:30:00 -0800

    adds new post functionality

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,25 +1,54 @@
 <template>
-  <div class="blog container">
-      <h2>{{ title }}</h2>
-      <hr>
-      <div class="row">
-       <div class="col-sm-6 col-md-4" v-for="post in posts">
-         <div class="thumbnail">
-           <img src="http://placehold.it/350x150">
-           <div class="caption">
-             <h3>{{post.title | uppercase}}</h3>
-             <p>
-               {{post.body}}
-               <a href="#" class="btn btn-danger" role="button" v-on:click="deletePost(post)">Delete</a></p>
-             </p>
-             <p>
-               <a href="#" class="btn btn-primary" role="button">Read More</a>
-               <a href="#" class="btn btn-danger" role="button">Delete</a>
-             </p>
+  <div>
+    <div class="blog container">
+        <h2>{{ title }}</h2>
+        <hr>
+        <div class="row">
+         <div class="col-sm-6 col-md-4" v-for="post in posts">
+           <div class="thumbnail">
+             <img src="http://placehold.it/350x150">
+             <div class="caption">
+               <h3>{{post.title | uppercase}}</h3>
+               <p>
+                 {{post.body}}
+                 <a href="#" class="btn btn-danger" role="button" v-on:click="deletePost(post)">Delete</a></p>
+               </p>
+               <p>
+                 <a href="#" class="btn btn-primary" role="button">Read More</a>
+                 <a href="#" class="btn btn-danger" role="button">Delete</a>
+               </p>
+             </div>
            </div>
          </div>
        </div>
-     </div>
+    </div>
+    <!-- Trigger the modal add post with a button -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addNewPost">New Post</button>
+    <!-- Add Post -->
+    <div id="addNewPost" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+        <!-- Add Post Content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Add New Post</h4>
+          </div>
+          <div class="modal-body">
+              <div class="form-group">
+                <label for="title">Title:</label>
+                <input type="title" class="form-control" id="title" v-model="newPost.title">
+              </div>
+              <div class="form-group">
+                <label for="body">Content:</label>
+                <textarea class="form-control" rows="5" id="body" v-model="newPost.body"></textarea>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal" v-on:click="AddPost()">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -52,12 +81,21 @@
       }
     },
     methods: {
-    deletePost: function (post) {
-      console.log(post)
-      this.posts.splice(this.posts.indexOf(post), 1)
+      deletePost: function (post) {
+        console.log(post)
+        this.posts.splice(this.posts.indexOf(post), 1)
+      },
+      newPost: function () {
+        this.posts.push({
+          'userId': 1,
+          'id': this.posts.length + 1,
+          'title': this.newPost.title,
+          'body': this.newPost.body
+        })
+        this.newPost = {}
       }
     }
-  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
